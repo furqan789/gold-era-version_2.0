53298671bef594586cb1a4a10c82f5fead8f6fcf
AtharvTam <62717473+AtharvTam@users.noreply.github.com>
2021-04-20 15:01:56 +0530

    login done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gold_price_predictor/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'MainPage.dart';
 import 'first.dart';
 import 'login.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home:Home(),
